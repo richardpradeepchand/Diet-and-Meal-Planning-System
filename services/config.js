12c1d28615278ec24568f2637a7edf8afe99d1d8
richardpradeepchand <20059649@mydbs.ie>
2025-12-07 23:57:17 +0000

    Create config.js

---
+++ services/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  PORT: 5000,
+  JWT_SECRET_KEY: "Richard123456",
+  MONGO_URL: "mongodb+srv://richardnuthalapati:richardnuthalapati@dietmaster.bbfibqo.mongodb.net/dietmaster?retryWrites=true&w=majority"
+};
