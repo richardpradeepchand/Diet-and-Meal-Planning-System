07bcbb175cfa16e25996a9751631f43ed1ae5140
richardpradeepchand <20059649@mydbs.ie>
2025-12-08 17:10:24 +0000

    Create User.js

---
+++ services/models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: {type: String,required: true, minlength: 6,},  role: { type: String, enum: ['user','admin'], default: 'user' },
+  dailyCalorieGoal: { type: Number, default: 2000 },
+  lastActive: { type: Date, default: Date.now } 
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', UserSchema);
